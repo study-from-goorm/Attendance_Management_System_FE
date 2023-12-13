b2b0326d7097852799231b3c0138b95d6d8dead9
oyeong011 <oyeong011@gmail.com>
2023-12-13 19:55:09 +0900

    Refactor : 라우터 구조 수정

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,8 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
 } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import ErrorPage from '../pages/Error';
@@ -18,71 +18,71 @@ import PlayerResultPage from '../pages/Player/Result';
 import AdminLayout from '../layouts/AdminLayout';
 import AdminMainPage from '../pages/Admin/Main';
 import PlayersInfo, {
-    loader as playersInfoLoader,
+  loader as playersInfoLoader,
 } from '../pages/Admin/Players/PlayersInfo';
 import AttendanceInfo from '../pages/Admin/Attendance/AttendanceInfo';
 import ApplyInfo from '../pages/Admin/Apply/ApplyInfo';
 import NewPlayer from '../pages/Admin/Players/NewPlayer';
 import EditPlayer, {
-    loader as editPlayerLoader,
-    action as editPlayerAction,
+  loader as editPlayerLoader,
+  action as editPlayerAction,
 } from '../pages/Admin/Players/EditPlayer';
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<MainLayout />} errorElement={<ErrorPage />}>
-            <Route element={<PublicRouter />}>
-                <Route path="login" element={<LoginPage />} action={loginAction} />
-            </Route>
-            <Route element={<PrivateRouter />}>
-                <Route
-                    path="player"
-                    element={
-                        <RoleRouter allowedRoles={['player', 'admin']}>
-                            <PlayerLayout />
-                        </RoleRouter>
-                    }
-                >
-                    <Route index element={<Navigate to="main" />} />
-                    <Route path="main" element={<PlayerMainPage />} />
-                    <Route path="apply" element={<PlayerApplyPage />} />
-                    <Route path="apply/result" element={<PlayerResultPage />} />
-                </Route>
-                <Route
-                    path="admin"
-                    element={
-                        <RoleRouter allowedRoles={['admin']}>
-                            <AdminLayout />
-                        </RoleRouter>
-                    }
-                >
-                    <Route index element={<Navigate to="main" />} />
-                    <Route path="main" element={<AdminMainPage />} />
-                    <Route
-                        id="player-course"
-                        path="players"
-                        element={<PlayersInfo />}
-                        loader={playersInfoLoader}
-                    >
-                        <Route
-                            path="new"
-                            element={<NewPlayer />}
-                            loader={playersInfoLoader}
-                        />
-                        <Route
-                            path=":id/edit"
-                            element={<EditPlayer />}
-                            loader={editPlayerLoader}
-                            action={editPlayerAction}
-                        />
-                    </Route>
-                    <Route path="attendances" element={<AttendanceInfo />} />
-                    <Route path="applys" element={<ApplyInfo />} />
-                </Route>
-                <Route path="logout" action={logoutAction} />
-            </Route>
-        </Route>,
-    ),
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />} errorElement={<ErrorPage />}>
+      <Route element={<PublicRouter />}>
+        <Route path="login" element={<LoginPage />} action={loginAction} />
+      </Route>
+      <Route element={<PrivateRouter />}>
+        <Route
+          path="player"
+          element={
+            <RoleRouter allowedRoles={['player', 'admin']}>
+              <PlayerLayout />
+            </RoleRouter>
+          }
+        >
+          <Route index element={<Navigate to="main" />} />
+          <Route path="main" element={<PlayerMainPage />} />
+          <Route path="apply" element={<PlayerApplyPage />} />
+          <Route path="apply/result" element={<PlayerResultPage />} />
+        </Route>
+        <Route
+          path="admin"
+          element={
+            <RoleRouter allowedRoles={['admin']}>
+              <AdminLayout />
+            </RoleRouter>
+          }
+        >
+          <Route index element={<Navigate to="main" />} />
+          <Route path="main" element={<AdminMainPage />} />
+          <Route
+            id="player-course"
+            path="players"
+            element={<PlayersInfo />}
+            loader={playersInfoLoader}
+          >
+            <Route
+              path="new"
+              element={<NewPlayer />}
+              loader={playersInfoLoader}
+            />
+            <Route
+              path=":id/edit"
+              element={<EditPlayer />}
+              loader={editPlayerLoader}
+              action={editPlayerAction}
+            />
+          </Route>
+          <Route path="attendances" element={<AttendanceInfo />} />
+          <Route path="applys" element={<ApplyInfo />} />
+        </Route>
+        <Route path="logout" action={logoutAction} />
+      </Route>
+    </Route>,
+  ),
 );
 
 export default router;
